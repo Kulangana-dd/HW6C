2b8c7cb14aa16fb3a832ea37f9ff815814ce79a8
unknown <kulangana@yandex.ru git config --global user.name Kulangana-ddgit --global user.email kulangana@yandex.ru>
2022-10-09 23:13:56 +0300

    task 41

---
+++ task41/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3*/
+
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.Write("Введите числo: ");
+        array[index] = Convert.ToInt32(Console.ReadLine());
+        index++;
+    }
+}
+
+void PrintArray(int[] finalArray)
+{
+    int count = finalArray.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{finalArray[position]} ");
+        position++;
+    }
+}
+
+
+int NumbersOverZero(int[] currentArray)
+{
+    int result = 0;
+    for (int i = 0; i < currentArray.Length; i++)
+    {
+        if (currentArray[i] > 0)
+        {
+            result = result + 1;
+        }
+    }
+    return result;
+}
+
+
+Console.Write("Сколько чисел вы хотите ввести: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[value];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int result = NumbersOverZero(array);
+Console.WriteLine($"Количество чисел больше 0 равно {result}");
